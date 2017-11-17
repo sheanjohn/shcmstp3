7bfea71bccfd881333dfa4c812fd73f5914e24b2
sheanjohn <sheanjohn@hotmail.com>
2017-11-17 16:49:02 +0800

    继续更新模块部分

--- admin/Common/Controller/PmodelController.class.php
+++ admin/Common/Controller/PmodelController.class.php
@@ -90,6 +90,8 @@ class PmodelController extends PpublicController {
 	    $d ['cloc'] = $_POST ['cloc'];
 	    $d ['ctype'] = $_POST ['ctype'];
 	    $d ['cinfo'] = $_POST ['cinfo'];
+	    $d ['useor'] = $_POST ['useor'];
+	    $d ['upic'] = $_POST ['upic'];
 	   
 	        if ($id != '0') {
 	            $resu = M ( "model_ctrls" )->where ( 'id=' . $id )->save ( $d );
--- admin/Common/Controller/PusermodelController.class.php
+++ admin/Common/Controller/PusermodelController.class.php
@@ -11,4 +11,22 @@ class PusermodelController extends PpublicController {
         $this->page_vals ();
         $this->page_valcount ();
     }
+    
+    /**
+     * 猎取(加载)模块,合并加入模块使用的控件
+     */
+    public function load_model(){
+        $id=$_POST['id'];
+        $resu=M('model')->where('id='.$id.' and isshow=1')->order('id desc')->find();
+        if($resu!=false){
+            $mcid=explode(',',$resu['mcid']);
+            foreach($mcid as $k => $v){
+                $ctrl=M('model_ctrls')->where('id='.$v)->order('id desc')->find();
+                $resu['ctrls'][$k]=$ctrl;
+            }
+            $this->ajaxReturn($resu);
+        }else{
+            echo 'err';
+        }
+    }
 }
